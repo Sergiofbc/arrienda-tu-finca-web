d8503beb562b908722758fd86e46159c426b13ff
Sergio Barreto <ba.sergio@javeriana.edu.co>
2024-08-20 19:05:37 -0500

    creacion proyecto

---
+++ ArriendaTuFinca/src/test/java/com/example/ArriendaTuFinca/ArriendaTuFincaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ArriendaTuFinca;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ArriendaTuFincaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
