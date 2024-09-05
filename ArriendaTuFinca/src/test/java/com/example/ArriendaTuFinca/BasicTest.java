51dff9261935f0b29689ff856f7689ad712f7611
Sergio Barreto <ba.sergio@javeriana.edu.co>
2024-09-05 03:21:17 -0500

    Agrega prueba básica

---
+++ ArriendaTuFinca/src/test/java/com/example/ArriendaTuFinca/BasicTest.java
@@ -0,0 +1,12 @@
+package com.example.ArriendaTuFinca;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class BasicTest {
+
+    @Test
+    public void testAlwaysPass() {
+        assertTrue(true);
+    }
+}
